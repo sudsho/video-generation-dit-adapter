a9d33135ab727547ccb32768e75644db16149fc4
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2026-04-05 19:14:56 +0530

    expose pipe from top-level

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,1 +1,3 @@
 from .motion_module import MotionModule, TemporalAttentionBlock
+from .integrated_pipe import IntegratedT2VPipe, GenerationConfig
+from .sd3_backbone import load_sd3_turbo, install_motion_hooks, remove_hooks
